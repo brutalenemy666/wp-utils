9cc3e111c36ebc2737525facb5801783b6d6025d
brutalenemy666 <brutalenemy666@gmail.com>
2015-12-15 22:11:00 +0200

    updating the acf wrappers

--- utils/class-acf.php
+++ utils/class-acf.php
@@ -23,6 +23,14 @@ class Crb_ACF {
 		return get_field($option_name, 'option');
 	}
 
+	public static function update_option( $option_name, $value ) {
+		if ( !self::exists() || !$option_name ) {
+			return;
+		}
+
+		update_field($option_name, $value, 'option');
+	}
+
 	public static function get_post_meta( $post_id=false, $meta_key, $format_value=true ) {
 		if ( !self::exists() || !$meta_key || !$post_id ) {
 			return;
@@ -31,12 +39,44 @@ class Crb_ACF {
 		return get_field($meta_key, $post_id, $format_value);
 	}
 
+	public static function update_post_meta( $post_id, $meta_key, $value ) {
+		if ( !self::exists() || !$meta_key || !$post_id ) {
+			return;
+		}
+
+		update_field($meta_key, $value, $post_id);
+	}
+
 	public static function get_term_meta( $term_obj, $meta_key ) {
 		if ( !self::exists() || !is_object($term_obj) ) {
 			return;
 		}
 
 		return get_field($meta_key, $term_obj);
+	}
+
+	public static function update_term_meta( $term_obj, $meta_key, $value ) {
+		if ( !self::exists() || !is_object($term_obj) ) {
+			return;
+		}
+
+		update_field($meta_key, $value, $term_obj);
+	}
+
+	public static function get_user_meta( $user_id, $meta_key ) {
+		if ( !self::exists() || !intval($user_id) ) {
+			return;
+		}
+
+		return get_field($meta_key, 'user_' . intval($user_id));
+	}
+
+	public static function update_user_meta( $user_id, $meta_key, $value ) {
+		if ( !self::exists() || !intval($user_id) ) {
+			return;
+		}
+
+		update_field($meta_key, $value, 'user_' . intval($user_id));
 	}
 
 	public static function get_widget_meta( $widget_id, $meta_key ) {
@@ -64,6 +104,20 @@ class Crb_ACF {
 			'redirect'     => true
 		), $params);
 
-		acf_add_options_page($params);
+		return acf_add_options_page($params);
+	}
+
+	public static function add_options_sub_page( $options_page, $params ) {
+		if ( !self::exists() || !function_exists('acf_add_options_sub_page') ) {
+			return;
+		}
+
+		$params = array_merge(array(
+			'page_title' 	=> __('Sub Options Page', 'crb'),
+			'menu_title' 	=> __('SubOptions', 'crb'),
+			'parent_slug' 	=> $options_page['menu_slug'],
+		), $params);
+
+		acf_add_options_sub_page($params);
 	}
 }
