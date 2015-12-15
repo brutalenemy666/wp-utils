29d6d1d42dc033bc1c35a51266ed85620d91b007
brutalenemy666 <brutalenemy666@gmail.com>
2015-12-15 22:25:17 +0200

    collecting the geolocation files into a single file

--- utils/class-geolocation.php
+++ utils/class-geolocation.php
@@ -4,7 +4,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-abstract class Crb_Abstract_Geolocation {
+class Crb_Geolocation {
 
 	protected $ip;
 	protected $address;
@@ -84,5 +84,97 @@ abstract class Crb_Abstract_Geolocation {
 		$return = filter_var($str, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4);
 
 		return $return;
+	}
+}
+
+class Crb_Geolocation_By_Address extends Crb_Geolocation {
+
+	protected $geocode_apis = 'http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false';
+
+	protected function _geolocate() {
+		$webservice_url = sprintf($this->geocode_apis, urlencode($this->address));
+
+		$result = wp_remote_get($webservice_url);
+		if( is_wp_error($result) ) {
+			$error = $result;
+			throw new Exception($error->get_error_message());
+		}
+
+		$geocode = json_decode($result['body']);
+
+		if ( !empty($geocode->results) ) {
+			$lat = $geocode->results[0]->geometry->location->lat;
+			$lng = $geocode->results[0]->geometry->location->lng;
+		} else {
+			$lat = 0;
+			$lng = 0;
+		}
+
+		$address = array(
+			'lat'     => $lat,
+			'lng'     => $lng,
+			'ip'      => false,
+			'address' => $this->address
+		);
+
+		return $address;
+	}
+}
+
+class Crb_Geolocation_By_IP extends Crb_Geolocation {
+
+	/** @var array API endpoints for geolocating an IP address */
+	protected $geoip_apis = array(
+		'telize'           => 'http://www.telize.com/geoip/%s',
+		'geoip-api.meteor' => 'http://geoip-api.meteor.com/lookup/%s',
+	);
+
+	protected function _geolocate() {
+		$lat = 0;
+		$lng = 0;
+
+		foreach ($this->geoip_apis as $service_name => $service_url) {
+			$webservice_url = sprintf($service_url, urlencode($this->ip));
+
+			$response = wp_remote_get($webservice_url, array(
+				'timeout' => 2
+			));
+
+			if ( is_wp_error($response) ) {
+				$error = $response;
+				// throw new Exception($error->get_error_message());
+				continue;
+			} else if ( !$response['body'] ) {
+				continue;
+			}
+
+			$geocode = json_decode($response['body']);
+
+			switch ($service_name) {
+				case 'telize':
+					$lat = $geocode->latitude;
+					$lng = $geocode->longitude;
+					break;
+				case 'geoip-api.meteor':
+					$lat = $geocode->ll[0];
+					$lng = $geocode->ll[1];
+					break;
+				default:
+					break;
+			}
+
+			if ( $lat!==0 || $lng!==0 ) {
+				break;
+			}
+		}
+
+		$address = array(
+			'lat'     => $lat,
+			'lng'     => $lng,
+			'ip'      => $this->ip,
+			'address' => false
+		);
+
+		return $address;
 	}
 }
