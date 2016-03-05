220c595784a22615470fc5f3a18ca015da691c35
Rashko Petrov <brutalenemy666@gmail.com>
2016-03-05 20:16:13 +0200

    function to retrieve the current page url, function to get the top most parent post entry by given post id

--- utils/class-func-helpers.php
+++ utils/class-func-helpers.php
@@ -11,7 +11,7 @@ class Crb_Helpers {
 	 * @param  string $defaults
 	 * @return a value if available from a POST request, otherwise returns a default value
 	 */
-	public static function read_post($post_name, $default='') {
+	public static function read_post( $post_name, $default = '' ) {
 		if ( !isset($_POST[$post_name]) || !$_POST[$post_name] ) {
 			return $default;
 		}
@@ -19,7 +19,7 @@ class Crb_Helpers {
 		return $_POST[$post_name];
 	}
 
-	public static function read_post_escaped($post_name, $default='') {
+	public static function read_post_escaped( $post_name, $default = '' ) {
 		$post_value = self::read_post($post_name, $default);
 
 		return esc_sql($post_value);
@@ -30,7 +30,7 @@ class Crb_Helpers {
 	 * @param  string $defaults
 	 * @return a value if available from a GET request, otherwise returns a default value
 	 */
-	public static function read_get($get_name, $default='') {
+	public static function read_get( $get_name, $default = '' ) {
 		if ( !isset($_GET[$get_name]) || !$_GET[$get_name] ) {
 			return $default;
 		}
@@ -38,7 +38,7 @@ class Crb_Helpers {
 		return $_GET[$get_name];
 	}
 
-	public static function read_get_escaped($post_name, $default='') {
+	public static function read_get_escaped( $post_name, $default = '' ) {
 		$get_value = self::read_get($post_name, $default);
 
 		return esc_sql($get_value);
@@ -72,7 +72,7 @@ class Crb_Helpers {
 		echo esc_html(self::read_post($field));
 	}
 
-	public static function checked_attribute($checked, $current='', $echo=false) {
+	public static function checked_attribute( $checked, $current = '', $echo = false ) {
 		return checked($checked, $current, $echo);
 	}
 
@@ -100,7 +100,7 @@ class Crb_Helpers {
 	 * @param  mixed $arr_obj
 	 * @return a value specified by a variable route from a given array or object
 	 */
-	public static function get_var($variable_route, $arr_obj, $default_value='') {
+	public static function get_var( $variable_route, $arr_obj, $default_value = '' ) {
 		if ( !$arr_obj ) {
 			return $default_value;
 		}
@@ -145,7 +145,7 @@ class Crb_Helpers {
 	 * @param  string $name
 	 * @return specific error message specified by an error name from a given errors array
 	 */
-	public static function maybe_display_error($errors=array(), $name='') {
+	public static function maybe_display_error( $errors = array(), $name = '' ) {
 		$errors = (array) $errors;
 
 		if ( empty($errors[$name]) ) {
@@ -167,7 +167,7 @@ class Crb_Helpers {
 	 * @param  string $end the suffix of the shortalized text
 	 * @return string
 	 */
-	public static function shortalize($input, $words_limit=15, $end='...') {
+	public static function shortalize( $input, $words_limit = 15, $end = '...' ) {
 		return wp_trim_words($input, $words_limit, $end);
 	}
 
@@ -176,7 +176,7 @@ class Crb_Helpers {
 	 * @param  mixed  $string
 	 * @return boolean         [description]
 	 */
-	public static function is_json($string) {
+	public static function is_json( $string ) {
 		json_decode($string);
 		return (json_last_error() == JSON_ERROR_NONE);
 	}
@@ -186,6 +186,42 @@ class Crb_Helpers {
 		$referer = self::read_server_var('HTTP_REFERER');
 		$origin = self::read_server_var('HTTP_ORIGIN');
 
-		return rtrim($request_uri, '/')===rtrim(str_replace($origin, '', $referer), '/');
+		return rtrim($request_uri, '/') === rtrim(str_replace($origin, '', $referer), '/');
+	}
+
+	/**
+	 * Returns the top most parent post
+	 */
+	public static function get_top_parent( $post_id = 0 ) {
+		$ancestors = get_post_ancestors($post_id);
+		$parents = array_reverse($ancestors);
+
+		if ( empty($parents) ) {
+			return get_post($post_id);
+		}
+
+		$top_parent = get_post($parents[0]);
+
+		return $top_parent;
+	}
+
+	/**
+	 * Returns the url of the current page
+	 */
+	public static function get_current_url() {
+		$page_url = 'http';
+
+		if ( !empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on" ) {
+			$page_url .= "s";
+		}
+
+		$page_url .= "://";
+		if ( $_SERVER["SERVER_PORT"] != "80" ) {
+			$page_url .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+		} else {
+			$page_url .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+		}
+
+		return $page_url;
 	}
 }
