f5b9a1a39cb08fc71e86eeff43aa5cffe706aeb2
Rashko Petrov <brutalenemy666@gmail.com>
2016-03-23 00:14:30 +0200

    removing a duplicated function

--- utils/class-helpers.php
+++ utils/class-helpers.php
@@ -233,24 +233,4 @@ class Crb_Helpers {
 
 		return $parents;
 	}
-
-	/**
-	 * Returns the url of the current page
-	 */
-	public static function get_current_url() {
-		$page_url = 'http';
-
-		if ( !empty($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on" ) {
-			$page_url .= "s";
-		}
-
-		$page_url .= "://";
-		if ( $_SERVER["SERVER_PORT"] != "80" ) {
-			$page_url .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
-		} else {
-			$page_url .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
-		}
-
-		return $page_url;
-	}
 }
